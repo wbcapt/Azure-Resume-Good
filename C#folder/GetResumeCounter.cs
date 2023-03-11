56e56c44f804c80f236440b868d05c0016b34cf9
wbcapt <baldcodes2@gmail.com>
2023-03-11 09:43:18 -0500

    changed the dotnet version in backend yml file and triggering action

--- C#folder/GetResumeCounter.cs
+++ C#folder/GetResumeCounter.cs
@@ -22,7 +22,7 @@ namespace Company.Function
 
             ILogger log)
         {
-            //adding a comment here to test backend, this where counter gets updated
+            //adding a comment here to test backend, this where counter gets updated. v2
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
