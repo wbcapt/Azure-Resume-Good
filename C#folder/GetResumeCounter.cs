1295708c363e14b5a8951771fe7e05ebe407a165
wbcapt <baldcodes2@gmail.com>
2023-03-11 10:37:24 -0500

    changed the build and deploy uses to checkout@main

--- C#folder/GetResumeCounter.cs
+++ C#folder/GetResumeCounter.cs
@@ -22,7 +22,7 @@ namespace Company.Function
 
             ILogger log)
         {
-            //adding a comment here to test backend, this where counter gets updated. v3
+            //adding a comment here to test backend, this where counter gets updated. v4
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
